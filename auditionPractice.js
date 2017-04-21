4cd7c05691d2e1789cf7af17bbece618142228ab
Shane English <shaneenglish646@gmail.com>
2017-04-20 22:32:04 -0400

    IT WORKS!

--- auditionPractice.js
+++ auditionPractice.js
@@ -1,12 +1,21 @@
-function doIt(){
-    var startNum = document.forms["getNums"]["startNum"].value;
-    console.log(startNum);
-    var secondNum = document.forms["getNums"]["secondNum"].value;
-    console.log(secondNum);
-    var stepNum = document.forms["getNums"]["stepNum"].value;
-    console.log(stepNum);
-    document.getElementById("showStart").innertext = startNum;
-    document.getElementById("showSecond").innertext = secondNum;
-    document.getElementById("showStep").innertext = stepNum;
+function readIt(){
+    var output = "";
+    var num1 = Number(document.forms["getNums"]["startNum"].value);
+        // console.log(num1);
+    var num2 = Number(document.forms["getNums"]["secondNum"].value);
+        // console.log(num2);
+    var num3 = Number(document.forms["getNums"]["stepNum"].value);
+        // console.log(num3);
+    for (var i = num1; i <= num2; i += num3){
+        // console.log(i);
+        if(i % 2 == 0){
+            output += " " + i + " ";
+        }
+    }
+    if(output == ""){ 
+            output = "There are no even values."
+        }
+    document.getElementById("results").style.display = 'block'
+    document.getElementById("showIt").innerText = output;
     return false;
 }
